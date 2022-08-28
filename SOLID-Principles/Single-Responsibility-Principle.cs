5d639c9e2b7cbff14c2290cdac4a08eaa2bf16c4
Shoaib Muhammad <smshoaib2001@gmail.com>
2022-08-28 12:31:47 +0500

    cc

---
+++ SOLID-Principles/Single-Responsibility-Principle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Principles
+{
+
+    interface IUser
+    {
+        bool Login(string _userName, string _password);
+        bool Register(string _userName, string _password);
+    }
+    interface ILogger
+    {
+        void Log(string message);
+    }
+    interface IEmail
+    {
+        void SendEmail(string message);
+    }
+    internal class Single_Responsibility_Principle
+    {
+    }
+}
